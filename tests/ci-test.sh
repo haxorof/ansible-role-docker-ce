3ddca904183cbc501907394bdfe389dc135182ee
Björn Oscarsson <haxorof@users.noreply.github.com>
2018-07-08 19:42:55 +0200

    Improved ci-test.sh

--- tests/ci-test.sh
+++ tests/ci-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
-if [[ ! -f yaml.sh ]]; then
+YAML_INC_FILE=yamlparser.sh.inc
+if [[ ! -f "$YAML_INC_FILE" ]]; then
   echo "Fetching YAML parsing script..."
-  wget -q https://raw.githubusercontent.com/jasperes/bash-yaml/master/yaml.sh
+  wget -O $YAML_INC_FILE -q https://raw.githubusercontent.com/jasperes/bash-yaml/master/yaml.sh
 fi
-. yaml.sh
+. $YAML_INC_FILE
 
 export CONFIG_KEY=
 UBUNTU_ON_WIN=$(uname -a | grep Microsoft)
@@ -22,98 +22,126 @@ BLDGRN='\033[1;32m'
 BLDBLU='\033[1;34m'
 BLDYLW='\033[1;33m' # Yellow
 BLDMGT='\033[1;35m' # Magenta
-BLDCYN='\033[1;36m' # Cyan 
+BLDCYN='\033[1;36m' # Cyan
 TXTRST='\033[0m'
 
-pass () {
+Done () {
+  printf "%b\n" "${BLDGRN}[DONE]${TXTRST} $1"
+}
+
+Pass () {
   printf "%b\n" "${BLDGRN}[PASS]${TXTRST} $1"
 }
 
-fail () {
+Fail () {
   printf "%b\n" "${BLDRED}[FAIL]${TXTRST} $1"
 }
 
-skip() {
+Skip() {
   printf "%b\n" "${BLDCYN}[SKIP]${TXTRST} $1"
 }
 
-redText() {
+RedText() {
   printf "%b\n" "${BLDRED}$1${TXTRST}"
 }
 
-vagrantExists() {
+VagrantExists() {
   if ! vagrant_loc="$(type -p $VAGRANT_CMD)" || [[ -z $vagrant_loc ]]; then
     echo "1"
   fi
   echo "0"
 }
 
-vagrantUp() {
-  if [[ "$(vagrantExists)" == "0" ]]; then
+VagrantUp() {
+  if [[ "$(VagrantExists)" == "0" ]]; then
     $VAGRANT_CMD up
     return $?
   else
-    redText "[vagrantUp] vagrant not found!"
+    RedText "[VagrantUp] vagrant not found!"
   fi
 }
 
-vagrantDestroy() {
-  if [[ "$(vagrantExists)" == "0" ]]; then
+VagrantDestroy() {
+  if [[ "$(VagrantExists)" == "0" ]]; then
     $VAGRANT_CMD destroy -f
     return $?
   else
-    redText "[vagrantDestroy] vagrant not found!"
+    RedText "[VagrantDestroy] vagrant not found!"
   fi
 }
 
-vagrantBoxAdd() {
-  if [[ "$(vagrantExists)" == "0" ]]; then
-    $VAGRANT_CMD box add $1
-    return $?
+VagrantBoxAdd() {
+  if [[ "$(VagrantExists)" == "0" ]]; then
+    cmdOutput=$($VAGRANT_CMD box add $1 2>&1)
+    exitCode=$?
+    if [[ "$cmdOutput" == *force* ]]; then
+      return 0
+    else
+      if [[ "$exitCode" != "0" ]]; then
+        RedText "$cmdOutput"
+      fi
+      return $exitCode
+    fi
   else
-    redText "[vagrantBoxAdd] vagrant not found!"
+    RedText "[VagrantBoxAdd] vagrant not found!"
   fi
   return 0
 }
 
+DownloadBoxes() {
+  echo "Downloading boxes..."
+  boxes=$(parse_yaml vagrant_config.yml | grep _box | cut -d= -f2 | sed 's/[\(\"\)]//g' | sed "s/'//g" | sort | uniq)
+  exitCode=0
+  for box in $boxes; do
+    if [[ "$box" != *"$LIMIT"* ]]; then
+      Skip "Download $box"
+      continue
+    fi
+    VagrantBoxAdd $box
+    if [[ "$?" == "0" ]]; then
+      Done "Download $box"
+    else
+      Fail "Download $box"
+      exitCode=1
+    fi
+  done
+  return $exitCode
+}
+
+ExecuteTests() {
+  echo "Starting tests..."
+  configs=$(parse_yaml vagrant_config.yml | grep _box | awk '{split($0,a,"_box"); $1=a[1]; split($1,b,"configs_"); $2=b[2];  print $2}')
+  exitCode=0
+  for config in $configs; do
+    CONFIG_KEY=$config
+    export CONFIG_KEY
+    if [[ "$CONFIG_KEY" != *"$LIMIT"* ]]; then
+      Skip "$CONFIG_KEY"
+      continue
+    fi
+    VagrantUp
+    exitCode=$?
+    VagrantDestroy
+    if [[ $exitCode == "0" ]]; then
+      Pass "Test $CONFIG_KEY"
+    else
+      Fail "Test $CONFIG_KEY"
+      break
+    fi
+  done
+  echo "Ended with exit code $exitCode"
+  return $exitCode
+}
+
 LIMIT=$1
 
-echo "Downloading boxes..."
-boxes=$(parse_yaml vagrant_config.yml | grep _box | cut -d= -f2 | sed 's/[\(\"\)]//g' | sed "s/'//g" | sort | uniq)
-for box in $boxes; do
-  if [[ "$box" != *"$LIMIT"* ]]; then
-    skip "Download $box"
-    continue
+if [[ "$SKIP_DOWNLOAD" == "" ]]; then
+  DownloadBoxes
+  echo "Download complete!"
+  if [[ "$DOWNLOAD_ONLY" != "" ]]; then
+    exit 0
   fi
-  vagrantBoxAdd $box
-  # A bit unstable downloads and if already downloaded exit code 1 is returned.
-  # So ignore exit code for now.
-  if [[ "$?" == "0" ]]; then
-    pass "Download $box"
-  else
-    pass "Download $box"
-  fi
-done
+fi
 
-echo "Starting tests..."
-configs=$(parse_yaml vagrant_config.yml | grep _box | awk '{split($0,a,"_box"); $1=a[1]; split($1,b,"configs_"); $2=b[2];  print $2}')
-exitCode=0
-for config in $configs; do
-  CONFIG_KEY=$config
-  export CONFIG_KEY
-  if [[ "$CONFIG_KEY" != *"$LIMIT"* ]]; then
-    skip "$CONFIG_KEY"
-    continue
-  fi
-  vagrantUp
-  exitCode=$?
-  vagrantDestroy
-  if [[ $exitCode == "0" ]]; then
-    pass "Test $CONFIG_KEY"
-  else
-    fail "Test $CONFIG_KEY"
-    break
-  fi
-done
-echo "Ended with exit code $exitCode"
-exit $exitCode
+ExecuteTests
+exit $?
